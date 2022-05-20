929c5d33f7b5a89ac5745ecf1f2513b11645b793
na9amura <4410504+na9amura@users.noreply.github.com>
2022-05-20 10:33:44 +0900

    remove ID column

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,35 +7,35 @@ type Role = [string, { ub: string }]
 const range = (n: number) => [...Array(n).keys()]
 const createRow = (cols: number): CellBase[]=> range(cols).map((i) => ({ value: undefined }))
 const dummyData = [
-  [{ value: 1 }, { value: 'Aaron'   }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 2 }, { value: 'Bob'     }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 3 }, { value: 'Chales'  }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 4 }, { value: 'Derrick' }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 5 }, { value: 'Eli'     }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 6 }, { value: 'Favre'   }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 7 }, { value: 'Gathie'  }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 8 }, { value: 'Hill'    }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 9 }, { value: 'Ingram'  }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Aaron'   }, { value: 'leader' }, { value: '' }, { value: '' }],
+  [{ value: 'Bob'     }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Chales'  }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Derrick' }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Eli'     }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Favre'   }, { value: 'leader' }, { value: '' }, { value: '' }],
+  [{ value: 'Gathie'  }, { value: 'leader' }, { value: '' }, { value: '' }],
+  [{ value: 'Hill'    }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: 'Ingram'  }, { value: 'member' }, { value: '' }, { value: '' }],
 ]
 const matrixToMemberList = (matrix: Matrix<CellBase>) => {
-  return matrix.flatMap((row) => {
+  return matrix.flatMap((row, i) => {
     return {
-      id: row[0]?.value,
-      role: row[2]?.value,
-      prev: row[3]?.value,
+      id: i,
+      role: row[1]?.value,
+      prev: row[2]?.value,
     }
   })
 }
 
 const App = () => {
-  const labels = ['ID', 'Name', 'Role', 'Previous Group', 'New Group']
+  const labels = ['Name', 'Role', 'Previous Group', 'New Group']
   const [matrix, setMatrix] = useState<Matrix<CellBase>>(dummyData);
   const [roles, setRoles] = useState<Role[]>([]);
-  const [numberOfGroups, setNumberOfGroups] = useState<string>('');
+  const [numberOfGroups, setNumberOfGroups] = useState<string>('1');
 
   useEffect(() => {
     const _roles = matrix
-      .map((row) => row[2]?.value)
+      .map((row) => row[1]?.value)
       .filter((v) => !!v)
       .filter((v, i, self) => self.indexOf(v) === i)
       .map<Role>((name) => [name, { ub: roles.find(([_name]) => _name === name)?.[1]?.ub ?? "" }])
@@ -66,9 +66,9 @@ const App = () => {
       roleConstraints,
     )
 
-    const _matrix = matrix.map((row) => {
-      const [, group = ""] = result.find(([id]) => id === row[0]?.value) ?? []
-      return [...row.slice(0, 4), { value: group }]
+    const _matrix = matrix.map((row, i) => {
+      const [, group = ""] = result.find(([id]) => id === i) ?? []
+      return [...row.slice(0, 3), { value: group }]
     })
     setMatrix(_matrix)
   }
