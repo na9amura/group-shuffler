a42fa6da04044e437c6b2e7ae35aa718cb75f3f2
na9amura <4410504+na9amura@users.noreply.github.com>
2022-05-21 00:08:22 +0900

    cleanup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react';
-import Spreadsheet, { CellBase, Matrix } from 'react-spreadsheet';
-import { solve } from './lp';
+import { useEffect, useState } from "react"
+import Spreadsheet, { CellBase, Matrix } from "react-spreadsheet"
+import { solve } from "./lp"
 
 type Role = [string, { ub: string }]
 
 const range = (n: number) => [...Array(n).keys()]
-const createRow = (cols: number): CellBase[]=> range(cols).map((i) => ({ value: undefined }))
+const createRow = (cols: number): CellBase[] =>
+  range(cols).map(() => ({ value: undefined }))
 const dummyData = [
-  [{ value: 'Aaron'   }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 'Bob'     }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 'Chales'  }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 'Derrick' }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 'Eli'     }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 'Favre'   }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 'Gathie'  }, { value: 'leader' }, { value: '' }, { value: '' }],
-  [{ value: 'Hill'    }, { value: 'member' }, { value: '' }, { value: '' }],
-  [{ value: 'Ingram'  }, { value: 'member' }, { value: '' }, { value: '' }],
+  [{ value: "Aaron" }, { value: "leader" }, { value: "" }, { value: "" }],
+  [{ value: "Bob" }, { value: "member" }, { value: "" }, { value: "" }],
+  [{ value: "Chales" }, { value: "member" }, { value: "" }, { value: "" }],
+  [{ value: "Derrick" }, { value: "member" }, { value: "" }, { value: "" }],
+  [{ value: "Eli" }, { value: "member" }, { value: "" }, { value: "" }],
+  [{ value: "Favre" }, { value: "leader" }, { value: "" }, { value: "" }],
+  [{ value: "Gathie" }, { value: "leader" }, { value: "" }, { value: "" }],
+  [{ value: "Hill" }, { value: "member" }, { value: "" }, { value: "" }],
+  [{ value: "Ingram" }, { value: "member" }, { value: "" }, { value: "" }],
 ]
 const matrixToMemberList = (matrix: Matrix<CellBase>) => {
   return matrix.flatMap((row, i) => {
@@ -28,11 +29,11 @@ const matrixToMemberList = (matrix: Matrix<CellBase>) => {
 }
 
 const App = () => {
-  const labels = ['Name', 'Role', 'Previous Group', 'New Group']
-  const [matrix, setMatrix] = useState<Matrix<CellBase>>(dummyData);
-  const [roles, setRoles] = useState<Role[]>([]);
-  const [lowerBuffer, setLowerBuffer] = useState<string>('3');
-  const [upperBuffer, setUpperBuffer] = useState<string>('3');
+  const labels = ["Name", "Role", "Previous Group", "New Group"]
+  const [matrix, setMatrix] = useState<Matrix<CellBase>>(dummyData)
+  const [roles, setRoles] = useState<Role[]>([])
+  const [lowerBuffer, setLowerBuffer] = useState<string>("3")
+  const [upperBuffer, setUpperBuffer] = useState<string>("3")
   const [numGroups, setNumGroups] = useState<number>(0)
 
   useEffect(() => {
@@ -40,14 +41,19 @@ const App = () => {
       .map((row) => row[1]?.value)
       .filter((v) => !!v)
       .filter((v, i, self) => self.indexOf(v) === i)
-      .map<Role>((name) => [name, { ub: roles.find(([_name]) => _name === name)?.[1]?.ub ?? "" }])
+      .map<Role>((name) => [
+        name,
+        { ub: roles.find(([_name]) => _name === name)?.[1]?.ub ?? "" },
+      ])
 
     setRoles(_roles)
   }, [matrix])
 
   useEffect(() => {
     const members = matrix.length
-    const numGroupSize = Math.floor((Number(lowerBuffer) + Number(upperBuffer)) / 2)
+    const numGroupSize = Math.floor(
+      (Number(lowerBuffer) + Number(upperBuffer)) / 2
+    )
     const numGroups = Math.floor(members / numGroupSize)
 
     setNumGroups(numGroups)
@@ -58,12 +64,16 @@ const App = () => {
     setMatrix([...matrix, row])
   }
   const setUb = (target: string, ub: string) => {
-    const _roles = roles.map<Role>(([name, value]) => target === name ? [name, { ...value, ub }] : [name, value])
+    const _roles = roles.map<Role>(([name, value]) =>
+      target === name ? [name, { ...value, ub }] : [name, value]
+    )
     setRoles(_roles)
   }
 
   const onSort = () => {
-    const _matrix = [...matrix].sort(([,,,a], [,,,b]) => a?.value.charCodeAt(0) - b?.value.charCodeAt(0))
+    const _matrix = [...matrix].sort(
+      ([, , , a], [, , , b]) => a?.value.charCodeAt(0) - b?.value.charCodeAt(0)
+    )
     setMatrix(_matrix)
   }
 
@@ -73,17 +83,14 @@ const App = () => {
     const roleConstraints = Object.fromEntries(
       roles
         .filter(([, { ub }]) => !!ub)
-        .map(([name, { ub, ...value }]) => [name, { ...value, ub: Number(ub)}]))
-
-    const result = await solve(
-      members,
-      roleConstraints,
-      {
-        list: groups,
-        lb: Number(lowerBuffer),
-        ub: Number(upperBuffer),
-      }
+        .map(([name, { ub, ...value }]) => [name, { ...value, ub: Number(ub) }])
     )
+
+    const result = await solve(members, roleConstraints, {
+      list: groups,
+      lb: Number(lowerBuffer),
+      ub: Number(upperBuffer),
+    })
 
     const _matrix = matrix.map((row, i) => {
       const [, group = ""] = result.find(([id]) => id === i) ?? []
@@ -97,25 +104,45 @@ const App = () => {
       <div>
         <h3>Members</h3>
         <div>
-          <Spreadsheet data={matrix} onChange={(matrix) => setMatrix(matrix)} columnLabels={labels} />
+          <Spreadsheet
+            data={matrix}
+            onChange={(matrix) => setMatrix(matrix)}
+            columnLabels={labels}
+          />
         </div>
         <button onClick={addRow}>Add Row</button>
       </div>
       <div>
         <h3>Config</h3>
-        {
-          roles.map(([name, value]) => (
-            <div key={name}>
-              <label>Max {name} in a group: </label>
-              <input type="number" min={9} max={100} value={value.ub} onChange={(e) => setUb(name, e.target.value)} />
-            </div>
-          ))
-        }
+        {roles.map(([name, value]) => (
+          <div key={name}>
+            <label>Max {name} in a group: </label>
+            <input
+              type="number"
+              min={9}
+              max={100}
+              value={value.ub}
+              onChange={(e) => setUb(name, e.target.value)}
+            />
+          </div>
+        ))}
         <div>
           <label>People in a group: </label>
-          <input type="number" min={0} max={50} value={lowerBuffer} onChange={(e) => setLowerBuffer(e.target.value)} />
+          <input
+            type="number"
+            min={0}
+            max={50}
+            value={lowerBuffer}
+            onChange={(e) => setLowerBuffer(e.target.value)}
+          />
           <span> ~ </span>
-          <input type="number" min={0}  max={50} value={upperBuffer} onChange={(e) => setUpperBuffer(e.target.value)} />
+          <input
+            type="number"
+            min={0}
+            max={50}
+            value={upperBuffer}
+            onChange={(e) => setUpperBuffer(e.target.value)}
+          />
           <span> Create {numGroups} groups </span>
         </div>
       </div>
@@ -123,6 +150,6 @@ const App = () => {
       <button onClick={onSort}>sort</button>
     </>
   )
-};
+}
 
 export default App
--- src/lp.ts
+++ src/lp.ts
@@ -1,92 +1,105 @@
-import factory, { GLPK, LP } from 'glpk.js'
+import factory, { GLPK, LP } from "glpk.js"
 
-type Member<Roles> = { id: number, role: Roles }
+type Member<Roles> = { id: number; role: Roles }
 type RoleConstraint = { [key: string]: { ub?: number } }
 type GLPK_Var = LP["objective"]["vars"][0]
 type GLPK_Subject = LP["subjectTo"][0]
 
-const subjects = <R extends RoleConstraint> (
-    glpk: GLPK,
-    X: [[number, string], GLPK_Var][],
-    members: Member<keyof R>[],
-    roleConstraints: R,
-    group: { list: string[], lb: number, ub: number }
+const subjects = <R extends RoleConstraint>(
+  glpk: GLPK,
+  X: [[number, string], GLPK_Var][],
+  members: Member<keyof R>[],
+  roleConstraints: R,
+  group: { list: string[]; lb: number; ub: number }
 ): GLPK_Subject[] => {
-    const subject1 = members.flatMap((m) => {
-        const m_g = X.filter(([[mId]]) => mId === m.id).map(([, v]) => v)
-        return {
-            name: 'member-should-be-assigned-to-one-group',
-            vars: m_g,
-            bnds: { type: glpk.GLP_FX, ub: 1.0, lb: 1.0 }
-        }
-    })
+  const subject1 = members.flatMap((m) => {
+    const m_g = X.filter(([[mId]]) => mId === m.id).map(([, v]) => v)
+    return {
+      name: "member-should-be-assigned-to-one-group",
+      vars: m_g,
+      bnds: { type: glpk.GLP_FX, ub: 1.0, lb: 1.0 },
+    }
+  })
 
-    const { lb, ub } = group
-    const bnds = lb === ub ? { type: glpk.GLP_FX, ub, lb } : { type: glpk.GLP_DB, ub, lb }
-    const subject2 = group.list.flatMap((g) => {
-        const g_m = X.filter(([[, gId]]) => gId === g).map(([, v]) => v)
-        return {
-            name: 'members-in-group-should-be-less-than-n',
+  const { lb, ub } = group
+  const bnds =
+    lb === ub ? { type: glpk.GLP_FX, ub, lb } : { type: glpk.GLP_DB, ub, lb }
+  const subject2 = group.list.flatMap((g) => {
+    const g_m = X.filter(([[, gId]]) => gId === g).map(([, v]) => v)
+    return {
+      name: "members-in-group-should-be-less-than-n",
+      vars: g_m,
+      bnds,
+    }
+  })
+
+  const subject3: GLPK_Subject[] = !roleConstraints
+    ? []
+    : group.list.flatMap((g) => {
+        return Object.entries(roleConstraints).map(([name, constraint]) => {
+          const roleMembers = members
+            .filter(({ role }) => role === name)
+            .map(({ id }) => id)
+          const g_m = X.filter(
+            ([[mId, gId]]) => gId === g && roleMembers.includes(mId)
+          ).map(([, v]) => v)
+          const ub = constraint.ub ?? members.length
+          return {
+            name: `${name}-should-be-less-than-${ub}`,
             vars: g_m,
-            bnds
-        }
-    })
-
-    const subject3: GLPK_Subject[] = !roleConstraints ? [] : (
-        group.list.flatMap((g) => {
-            return Object.entries(roleConstraints).map(([name, constraint]) => {
-                const roleMembers = members.filter(({ role }) => role === name).map(({ id }) => id)
-                const g_m = X.filter(([[mId, gId]]) => gId === g && roleMembers.includes(mId)).map(([, v]) => v)
-                const ub = constraint.ub ?? members.length
-                return {
-                    name: `${name}-should-be-less-than-${ub}`,
-                    vars: g_m,
-                    bnds: { type: glpk.GLP_DB, ub, lb: 0.0 }
-                }
-            })
+            bnds: { type: glpk.GLP_DB, ub, lb: 0.0 },
+          }
         })
-    )
+      })
 
-    return [
-        ...subject1,
-        ...subject2,
-        ...subject3,
-    ]
+  return [...subject1, ...subject2, ...subject3]
 }
 
-const varName = (id: Member<{}>['id'], groupName: string) => `${id}-${groupName}`
+const varName = (id: Member<[]>["id"], groupName: string) =>
+  `${id}-${groupName}`
 
-export const solve = async <R extends RoleConstraint> (
-    _members: Member<keyof R>[],
-    roleConstraints: R,
-    group: { list: string[], lb: number, ub: number }
+export const solve = async <R extends RoleConstraint>(
+  _members: Member<keyof R>[],
+  roleConstraints: R,
+  group: { list: string[]; lb: number; ub: number }
 ): Promise<[id: number, group: string][]> => {
-    const members = _members.sort(() => Math.random() - 0.5)
+  const members = _members.sort(() => Math.random() - 0.5)
 
-    const X = members.flatMap((m) => group.list.map<[[number, string], GLPK_Var]>((g) => [[m.id, g], { name: varName(m.id, g), coef: 1.0 }]))
-    const vars = X.map(([, v]) => v)
-    const binaries = vars.map((v) => v.name)
+  const X = members.flatMap((m) =>
+    group.list.map<[[number, string], GLPK_Var]>((g) => [
+      [m.id, g],
+      { name: varName(m.id, g), coef: 1.0 },
+    ])
+  )
+  const vars = X.map(([, v]) => v)
+  const binaries = vars.map((v) => v.name)
 
-    // @ts-expect-error : 2349
-    const glpk: GLPK = await factory()
-    const params = {
-        name: 'LP',
-        objective: {
-            direction: glpk.GLP_MAX,
-            name: 'group',
-            vars,
-        },
-        binaries,
-        subjectTo: subjects(glpk, X, members, roleConstraints, group)
-    }
-    const options = {
-        msglev: glpk.GLP_MSG_ALL,
-        presol: true,
-    }
-    const { result } = await glpk.solve(params, options);
+  // @ts-expect-error : 2349
+  const glpk: GLPK = await factory()
+  const params = {
+    name: "LP",
+    objective: {
+      direction: glpk.GLP_MAX,
+      name: "group",
+      vars,
+    },
+    binaries,
+    subjectTo: subjects(glpk, X, members, roleConstraints, group),
+  }
+  const options = {
+    msglev: glpk.GLP_MSG_ALL,
+    presol: true,
+  }
+  const { result } = await glpk.solve(params, options)
 
-    return members
-        .flatMap((m) => group.list.map<[number, string, number]>((g) => [m.id, g, result.vars[varName(m.id, g)]]))
-        .filter(([, , assign]) => assign === 1)
-        .map(([id, group]) => [id, group])
+  return members
+    .flatMap((m) =>
+      group.list.map<[number, string, number]>((g) => [
+        m.id,
+        g,
+        result.vars[varName(m.id, g)],
+      ])
+    )
+    .filter(([, , assign]) => assign === 1)
+    .map(([id, group]) => [id, group])
 }
